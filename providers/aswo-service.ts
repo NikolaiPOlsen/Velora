f7de2ef8e494caec272c2ea16e8bdb47446560f9
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-12 02:30:46 +0200

    Remove unused fetchAswoData function from aswo-service.ts

--- providers/aswo-service.ts
+++
@@ -1,8 +0,0 @@
-export const fetchAswoData = async (search: string) => {
-    const key = process.env.EXPO_PUBLIC_ASWO_KEY;
-    const response = await fetch(`https://aswoshop.aswo.com/service/customerapi/devicesearch?devicesearchterms=${search}&format=FFFF?key=${key}`, {
-        method: 'GET',
-        headers: { "Content-Type": "application/json" },
-    });
-    return await response.json();
-}
