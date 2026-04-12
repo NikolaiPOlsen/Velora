c4f09194912a12042aada3fcb777139820c34abe
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-12 02:30:04 +0200

    Export aswoSupplier and update fetchParts parameter name for clarity

--- services/Suppliers/aswo-service.ts
+++ services/Suppliers/aswo-service.ts
@@ -1,12 +1,12 @@
 import { PartsSupplier } from './types';
 
-const aswoSupplier: PartsSupplier = {
-    fetchParts: async (search: string) => {
+export const aswoSupplier: PartsSupplier = {
+    fetchParts: async (query: string) => {
     const key = process.env.EXPO_PUBLIC_ASWO_KEY;
-    const response = await fetch(`https://aswoshop.aswo.com/service/customerapi/devicesearch?devicesearchterms=${search}&format=FFFF?key=${key}`, {
+    const response = await fetch(`https://aswoshop.aswo.com/service/customerapi/devicesearch?devicesearchterms=${query}&format=FFFF?key=${key}`, {
         method: 'GET',
         headers: { "Content-Type": "application/json" },
     });
     return await response.json();
-}
+},
 };
